405030e072903ea4d52cf85faa4b8696503d3aca
Anderson Sales <andersonsales.mg@gmail.com>
2023-03-06 23:13:52 -0300

    Implements Alamofire method

---
+++ ChatApp/Utilities/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Anderson Sales on 06/03/23.
+//
+
+import Foundation
+
+// Closure
+typealias completionHandler = (_ success: Bool) -> ()
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// Url Constants
+let BASE_URL = ""
+let URL_REGISTER = "\(BASE_URL)/account/register"
